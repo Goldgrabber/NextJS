c42b32553a0b672266078f1555deac5106d0ed2e
Goldgrabber <goldgraber@gmail.com>
2022-06-01 16:43:48 +0300

    typescript tipes

--- test.js
+++ test.js
@@ -1,2 +1,5 @@
 "use strict";
 var a = 5;
+var b = 'text';
+var c = true;
+var d = ['werw', 'ewrq', 'dsf'];
--- test.ts
+++ test.ts
@@ -1,1 +1,32 @@
 const a: number = 5;
+
+let b: string = 'text';
+
+let c = true;
+
+const d: string[] = ['werw', 'ewrq', 'dsf'];
+
+let e: any = 5;
+e = 'dsf';
+
+function test(a: string): number | string {
+	return '';
+}
+
+const test2 = (a: number): number => { return a * 2; }
+
+d = d.map((x: string) => x.toLowerCase())
+
+function countCoord(coord: { lat: number, long?: number }) {
+
+}
+function printIt(id: number | string) {
+	if (typeof id === 'number') {
+		console.log(id.toString());
+	} else if (typeof id === 'string') {
+		id.toUpperCase()
+	}
+}
+
+const x: undefined = undefined;
+const z: null = null;
